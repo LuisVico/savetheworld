bbf679739b6da9dbe49d5d73a0efde91e1f8d110
Luis Vg <luisvico@macbook-de-luis.local>
2018-01-30 14:23:26 +0100

    Initial Commit

---
+++ savethekids/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  savethekids
+//
+//  Created by Luis Vico on 30/1/18.
+//  Copyright © 2018 Luis Vico. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
